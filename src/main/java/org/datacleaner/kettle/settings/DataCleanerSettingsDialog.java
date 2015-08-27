88dab67501d99888da6d6376243dd17d98a89dcc
ClaudiaPHI <claudia.pesu@humaninference.com>
2015-08-27 16:55:37 +0200

    fixed the margin of the grid

--- src/main/java/org/datacleaner/kettle/settings/DataCleanerSettingsDialog.java
+++ src/main/java/org/datacleaner/kettle/settings/DataCleanerSettingsDialog.java
@@ -84,21 +84,33 @@ public class DataCleanerSettingsDialog extends Dialog {
         _shell = new Shell(getParent(), getStyle());
         _shell.setSize(500, 425);
         _shell.setText(getText());
-        _shell.setLayout(new GridLayout(2, false));
+        final GridLayout gridLayout = new GridLayout(3, false);
+        gridLayout.marginLeft = -5;
+        gridLayout.marginRight = -5;
+        gridLayout.marginTop = -5;
+        gridLayout.marginBottom = -5;
+        
+        
+        _shell.setLayout(gridLayout);
         _shell.setOrientation(getStyle());
         final DataCleanerBanner banner = new DataCleanerBanner(_shell);
-        banner.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        banner.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 3, 1));
 
+        
         new Label(_shell, SWT.NONE);
         new Label(_shell, SWT.NONE);
+        new Label(_shell, SWT.NONE);
 
+        new Label(_shell, SWT.NONE);
         final Label lblSelectDatacleanerApplication = new Label(_shell, SWT.NONE);
         lblSelectDatacleanerApplication.setText("Select DataCleaner application directory:");
         new Label(_shell, SWT.NONE);
 
         new Label(_shell, SWT.NONE);
         new Label(_shell, SWT.NONE);
+        new Label(_shell, SWT.NONE);
 
+        new Label(_shell, SWT.NONE);
         final Text _text = new Text(_shell, SWT.BORDER);
         _text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
         final Button btnBrowse = new Button(_shell, SWT.NONE);
@@ -106,7 +118,9 @@ public class DataCleanerSettingsDialog extends Dialog {
 
         new Label(_shell, SWT.NONE);
         new Label(_shell, SWT.NONE);
-
+        new Label(_shell, SWT.NONE);
+        
+        new Label(_shell, SWT.NONE);
         final Label errorLabel = new Label(_shell, SWT.NONE);
         errorLabel.setText("The selected folder does not contain a DataCleaner installation");
         errorLabel.setForeground(getParent().getDisplay().getSystemColor(SWT.COLOR_RED));
@@ -115,18 +129,23 @@ public class DataCleanerSettingsDialog extends Dialog {
 
         new Label(_shell, SWT.NONE);
         new Label(_shell, SWT.NONE);
+        new Label(_shell, SWT.NONE);
 
+        new Label(_shell, SWT.NONE);
         final Label labelEdition = new Label(_shell, SWT.NONE);
         labelEdition.setText(EDITION);
         new Label(_shell, SWT.NONE);
-
+        
+        new Label(_shell, SWT.NONE);
         final Label labelVersion = new Label(_shell, SWT.NONE);
         labelVersion.setText(VERSION);
         new Label(_shell, SWT.NONE);
 
         new Label(_shell, SWT.NONE);
         new Label(_shell, SWT.NONE);
+        new Label(_shell, SWT.NONE);
 
+        new Label(_shell, SWT.NONE);
         new Label(_shell, SWT.NONE);
         final Button btnOK = new Button(_shell, SWT.NONE);
         btnOK.setText("OK");
@@ -138,7 +157,7 @@ public class DataCleanerSettingsDialog extends Dialog {
         });
 
         final DataCleanerFooter footer = new DataCleanerFooter(_shell);
-        footer.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+        footer.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 3, 1));
 
         btnBrowse.addSelectionListener(new SelectionAdapter() {
             public void widgetSelected(SelectionEvent event) {
